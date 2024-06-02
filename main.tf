6ee03d26f83b8c4a3cb719d772bb4c532a1039a1
Vivekyadav00 <166670631+Vivekyadav00@users.noreply.github.com>
2024-06-02 16:50:16 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance to launch"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
